dacdbe60bbb6e7c422afa67da28bd270743c2ad0
SpunkyAardvark123 <ktsanter@gmail.com>
2018-04-20 20:00:52 -0400

    iframe working, removed red border

--- scripts/flipper.js
+++ scripts/flipper.js
@@ -68,7 +68,6 @@ var ktsFlipperCode = {
 		],
 
 	"prepareFlipperIframe": function(elemWrapperId, inputParameters) {
-			console.log('begin prepareFlipperIframe');
 			var html = this.iframeHTMLContent;
 			html = html.replace('***input_parameters***', JSON.stringify(inputParameters));
 			var iframe = document.createElement('iframe');
@@ -79,12 +78,10 @@ var ktsFlipperCode = {
 			var iframeWrapper = document.getElementById(elemWrapperId);
 			iframeWrapper.innerHTML = "";
 			iframeWrapper.appendChild(iframe);
-			console.log('end prepareFlipperIframe');
 	},
 		
 	"prepareFlipper": function(inputParameters)
 		{
-			console.log('begin prepareFlipper');
 			this.loadParameters(inputParameters);
 			this.loadFlipperCSS();
 		},
@@ -98,7 +95,6 @@ var ktsFlipperCode = {
 			for (var i = 0; i < inputParameters.images.length; i++) {
 				this.param.images[i] = inputParameters.images[i];
 			}
-			console.log('end loadParameters');
 		},
 	
 	"loadFlipperCSS": function()
@@ -109,7 +105,6 @@ var ktsFlipperCode = {
 			
 			xhttp.onreadystatechange = function() {
 				if (this.readyState == 4 && this.status == 200) {
-					console.log('success loadFlipperCSS');
 					var styleElement = document.createElement('style');
 					styleElement.innerHTML = xhttp.responseText;
 					document.head.appendChild(styleElement);
@@ -134,19 +129,15 @@ var ktsFlipperCode = {
 	
 	"loadHTML": function()
 		{
-			console.log('begin loadHTML');
 			var wrapper = document.getElementById(this.param.mainContentId);
 			wrapper.innerHTML = this.baseHTML;
 			wrapper.classList.add(this.colorSchemeClasses[this.param.colorscheme]);
-			console.log('end loadHTML');
 		},
 
 	"loadDescription": function()
 		{  
-			console.log('begin loadDescription');
 			document.getElementById('ktsFlipperTitle').textContent = this.param.title;
 			document.getElementById('ktsFlipperSubtitle').textContent = this.param.subtitle;
-			console.log('end loadDescription');
 		},
 
 	"loadMainCard": function()
